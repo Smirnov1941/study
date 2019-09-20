b0c23e22cd3441c5cb48c09f06800edf88aab357
Smirnov1941 <55545781+Smirnov1941@users.noreply.github.com>
2019-09-20 19:04:37 +0300

    Update Task2.py

--- Homework_01/Task2.py
+++ Homework_01/Task2.py
@@ -3,7 +3,7 @@ def pal(n):
         print("YES")
     elif n<100:
         print("NO")
-    elif n<1000 and (n%100=0 or (n%10=0 and n%11=0)):
+    elif n<1000 and (n%100==0 or (n%10==0 and n%11==0)):
         print("YES")
     elif n==n//1000+((n-((n//1000)*1000))//100)*10+((n-((n-((n//1000)*1000))//100)*100-(n//1000)*1000)//10)*100+n%10*1000:
         print("YES")
